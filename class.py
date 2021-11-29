bc6a47e5536d54328aac706b99ba619dd2edae78
Elton Z <poptako9@gmail.com>
2021-11-29 13:09:31 -0800

    Adding on the fish class
    I am adding on the fish class and some of the methods along with some of
    the data attritubes needed for th fish class.

--- class.py
+++ class.py
@@ -1,1 +1,18 @@
-
+#Assignment 10.1 Create your own class
+#By Elton Zeng
+#The goal of the assignment is to create your own class and some methods of that class
+class Fish():
+    def __init__(self):
+        self.__fin = 0
+        self.__gills = 2
+        self.__swim = True  
+        self.__position = 0
+        self.__size = 0
+    def set_fish(self):
+        pass 
+    def get_fish(self):
+        pass
+    def swim_forward(self):
+        pass
+    def get_eaten(self):
+        pass 
