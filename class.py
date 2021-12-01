751841bf4c91b5019b534f08bbcb944ab6f24ad6
Elton Z <poptako9@gmail.com>
2021-11-30 22:45:24 -0800

    Added on more to cover cases
    I added on more if statements to cover for errors that can be expected
    to occur along with fixing some syntax errors that occured while I was
    typing out the script. This is one of the final changes and the program
    runs as it was intended to.

--- class.py
+++ class.py
@@ -34,24 +34,34 @@ class Fish():
         #position cannot be negative 
         if new_pos < 0:
             raise ValueError("Position cannot be less than zero")
+        elif self.get_status() == False:
+            #return an f string that says you can't move the dead fish 
+            print(f"Unable to move dead fish {self.get_name()}.")
         else:
             self.__position = new_pos
     def get_position(self):
         return self.__position
     #set the number of fins for speed
     def set_fin(self, num_fins):
-        self.__fin = num_fins 
+        if num_fins < 0:
+            raise ValueError("Fins cannot be less than zero")
+        else:
+            self.__fin = num_fins 
     def get_fin(self):
         return self.__fin
     #set speed of fish
     def set_speed(self,speed):
-        #speed of the fish is based on the size and the number of fins
-        self.__speed = self.get_size() + speed + self.get_fin()  
+        if speed < 0:
+            raise ValueError("Speed cannot be less than zero")
+        else:
+            #speed of the fish is based on the size and the number of fins
+            self.__speed = self.get_size() + speed + self.get_fin()  
     def get_speed(self):
         return self.__speed
     #set the status of the fish 
     def set_status(self, status):
         if status == True or status == False:
+            #if it is true, then the fish is alive, otherwise, it is dead
             self.__alive = status 
     def get_status(self):
         return self.__alive
@@ -82,7 +92,7 @@ class Fish():
             return f"{self.get_name()} can't swim. Quite ironic."
         #and if the get_status is false, the fish is dead and can't do anything
         else:
-            return f"{self.get_name()} is dead. :("
+            return f"Unable to swim as {self.get_name()} is dead. :("
 
     def swim_back(self, num_secs):
         #if it can swim and the fish is alive then move forward
@@ -102,12 +112,12 @@ class Fish():
             return f"{self.get_name()} can't swim. Quite ironic."
         #and if the get_status is false, the fish is dead and can't do anything
         else:
-            return f"{self.get_name()} is dead. :("
+            return f"Unable to swim as {self.get_name()} is dead. :("
 
-    #pass through another fish object into this method
+    #pass through another fish object into this method 
     def consume(self, food):
         #if the size of the fish is greater than the size of the other fish (food), then eat it
-        if self.get_size() > food.get_size(): 
+        if self.get_size() > food.get_size() and self.get_status() == True: 
             #add the size of the fish and the one it is eating together
             self.__incsize = self.get_size() + food.get_size()
             #the size of the food fish is 0 cuz it just got eaten
@@ -117,6 +127,12 @@ class Fish():
             self.set_size(self.__incsize)
             #return the f string saying that this fish ate the food and the size of the fish now
             return f'{self.get_name()} has successfully eaten {food.get_name()} and the size of {self.get_name()} is now {self.get_size()}.'
+        #if the size of the fish is equal, then it will be unsuccessful and both fish will still be alive
+        elif self.get_size() == food.get_size() and self.set_status() == True:
+            return f'{self.get_name()} is unable to eat {food.get_name} as it is the same size.'
+        #in case the user tries to get a dead fish to eat some other fish.
+        elif self.get_status() == False:
+            return f'{self.get_name()} is dead and cannot eat {food.get_name()}.'
         #otherwise, the food will eat the other fish due to size difference
         else:
             #add the size of the food fish to the fish that first tried to eat it
@@ -125,6 +141,7 @@ class Fish():
             self.set_size(0)
             #this fish is now dead
             self.set_status(False)
+            self.set_swim(False)
             #return the f string saying that the food ate the other fish and the size of the food fish now
             return f'Oh no, {food.get_name()} has eaten {self.get_name()} and size of {food.get_name()} is now {food.get_size()}.'
     
@@ -134,11 +151,11 @@ class Fish():
         print(f'Size: {self.get_size()}')
         print(f'Number of fin(s): {self.get_fin()}')
         print(f'Speed: {self.get_speed()}')
-        if self.get_status == True:
+        if self.get_status() == True:
             print("Status: Alive")
         else:
             print("Status: Dead")
-        if self.get_swim == True:
+        if self.get_swim() == True:
             print("Can Swim")
         else:
             print("Can't Swim")
@@ -155,7 +172,7 @@ def main():
     fish1.set_fin(2)
     fish1.set_speed(3)
     fish1.set_status(True)
-    print(fish1.swim_forward(3))
+    print(fish1.swim_forward(1))
     #set the stats of the second fish that will be eaten 
     fish2.set_size(5)
     #show the results of fish1 eating fish2
@@ -169,6 +186,9 @@ def main():
     fish3.set_size(50)
     fish4.set_size(90)
     print(fish3.consume(fish4))
+    #print out the case where a dead fish tries to eat something
+    print(fish3.consume(fish2))
+    print(fish3.set_position(3))
     
 
 if __name__ == "__main__":
